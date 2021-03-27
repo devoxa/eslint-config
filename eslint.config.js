f5b4a62245594f955a81780f6c1c74c3740ff66c
queicherius <queicherius@gmail.com>
2021-03-27 09:11:18 +0000

    Disable react/prop-types

--- eslint.config.js
+++ eslint.config.js
@@ -17,15 +17,29 @@ module.exports = {
     jest: true,
   },
   rules: {
+    // Don't require a weird naming convention for interfaces
     '@typescript-eslint/interface-name-prefix': 'off',
+
+    // Allow inferring types for functions
     '@typescript-eslint/explicit-function-return-type': 'off',
+
+    // Allow inferring types for exported things
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+    // Don't allow "any" at all
     '@typescript-eslint/no-explicit-any': 2,
+
+    // Allow using functions that are defined later in the file (fine thanks to hoisting)
     '@typescript-eslint/no-use-before-define': [
       'error',
       { functions: false, classes: false, variables: true },
     ],
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+    // This rule makes no difference - ' and " are fine, > and } are already compile errors
     'react/no-unescaped-entities': 0,
+
+    // Disable validation of prop types, because we use TS instead and it gets confused
+    'react/prop-types': 0,
   },
   settings: {
     react: {
