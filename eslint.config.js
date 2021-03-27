238b10ed6051f5a94380b9a41c6cdedb2f48f6e4
queicherius <queicherius@gmail.com>
2021-03-27 05:58:59 +0000

    Remove wrong prettier plugin

--- eslint.config.js
+++ eslint.config.js
@@ -8,8 +8,8 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
     'plugin:react/recommended',
+    'prettier',
   ],
   root: true,
   env: {
