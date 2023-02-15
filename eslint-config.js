c268d1654f93961eb4e1889a54899afc51039fb2
David Reess <david@reess.me>
2023-02-15 22:56:35 +0000

    Revert "Add jsx-runtime plugin"

--- eslint-config.js
+++ eslint-config.js
@@ -9,7 +9,6 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
     'prettier',
   ],
   root: true,
