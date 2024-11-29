c319b8b0784ec8ba5e3494349a32ab2000414dbb
queicherius <queicherius@gmail.com>
2024-11-29 18:19:22 +0000

    Change config format to v9

--- eslint-config.js
+++ eslint-config.js
@@ -1,73 +1,106 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-    warnOnUnsupportedTypeScriptVersion: false,
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'prettier',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  rules: {
-    // Don't require a weird naming convention for interfaces
-    '@typescript-eslint/interface-name-prefix': 'off',
+const fs = require('fs')
+const jsEslint = require('@eslint/js')
+const tsEslint = require('typescript-eslint')
+const parseGitignore = require('parse-gitignore')
+const eslintConfigPrettier = require('eslint-config-prettier')
+const eslintPluginReact = require('eslint-plugin-react/configs/recommended')
 
-    // Allow inferring types for functions
-    '@typescript-eslint/explicit-function-return-type': 'off',
+module.exports = function (options) {
+  const { ignoreFiles = [], configs = [] } = options
 
-    // Allow inferring types for exported things
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  return tsEslint.config(
+    // Transform ignore files (e.g. gitignore) into ignore configs
+    ignoreFilesToIgnoreConfigs(ignoreFiles),
+    { ignores: ['eslint.config.js'] },
 
-    // Don't allow "any" at all
-    '@typescript-eslint/no-explicit-any': 'error',
-
-    // Make sure we are awaiting or explicitly voiding all Promises
-    '@typescript-eslint/no-floating-promises': 'error',
-
-    // Allow using functions that are defined later in the file (fine thanks to hoisting)
-    '@typescript-eslint/no-use-before-define': [
-      'error',
-      { functions: false, classes: false, variables: true },
-    ],
-
-    // This rule makes no difference - ' and " are fine, > and } are already compile errors
-    'react/no-unescaped-entities': 'off',
-
-    // Disable validation of prop types, because we use TS instead and it gets confused
-    'react/prop-types': 'off',
-
-    // Make sure comments are starting with an uppercase letter, to encourage correct grammar
-    'capitalized-comments': [
-      'warn',
-      'always',
-      {
-        ignorePattern: 'prettier|c8',
-        ignoreConsecutiveComments: true,
+    // Recommended config for linting with type information
+    jsEslint.configs.recommended,
+    ...tsEslint.configs.recommendedTypeChecked,
+    {
+      languageOptions: {
+        parserOptions: {
+          project: 'tsconfig.json',
+          sourceType: 'module',
+          warnOnUnsupportedTypeScriptVersion: false,
+        },
       },
-    ],
-
-    // Disable specific syntax features
-    'no-restricted-syntax': [
-      'error',
-      // Don't allow TS enums at all, since they have multiple unintuitive footguns
-      {
-        selector: 'TSEnumDeclaration',
-        message: 'Unexpected enum. Use a literal string union or a const object instead.',
-      },
-    ],
-  },
-  settings: {
-    react: {
-      version: 'detect',
     },
-  },
+
+    // Linting with React/JSX support
+    eslintPluginReact,
+    { settings: { react: { version: 'detect' } } },
+
+    // Disable rules that conflict with Prettier
+    eslintConfigPrettier,
+
+    // Custom rule overrides
+    {
+      rules: {
+        // Don't require a weird naming convention for interfaces
+        '@typescript-eslint/interface-name-prefix': 'off',
+
+        // Allow inferring types for functions
+        '@typescript-eslint/explicit-function-return-type': 'off',
+
+        // Allow inferring types for exported things
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+        // Don't allow "any" at all
+        '@typescript-eslint/no-explicit-any': 'error',
+
+        // Make sure we are awaiting or explicitly voiding all Promises
+        '@typescript-eslint/no-floating-promises': 'error',
+
+        // Allow using functions that are defined later in the file (fine thanks to hoisting)
+        '@typescript-eslint/no-use-before-define': [
+          'error',
+          { functions: false, classes: false, variables: true },
+        ],
+
+        // This rule makes no difference - ' and " are fine, > and } are already compile errors
+        'react/no-unescaped-entities': 'off',
+
+        // Disable validation of prop types, because we use TS instead and it gets confused
+        'react/prop-types': 'off',
+
+        // Make sure comments are starting with an uppercase letter, to encourage correct grammar
+        'capitalized-comments': [
+          'warn',
+          'always',
+          { ignorePattern: 'prettier|c8', ignoreConsecutiveComments: true },
+        ],
+
+        // Disable specific syntax features
+        'no-restricted-syntax': [
+          'error',
+          // Don't allow TS enums at all, since they have multiple unintuitive footguns
+          {
+            selector: 'TSEnumDeclaration',
+            message: 'Unexpected enum. Use a literal string union or a const object instead.',
+          },
+        ],
+      },
+    },
+
+    // Any additional configs that are passed in
+    ...configs
+  )
+}
+
+function ignoreFilesToIgnoreConfigs(ignoreFiles) {
+  const ignores = []
+
+  for (const path of ignoreFiles) {
+    const content = fs.readFileSync(path, 'utf8')
+
+    const parsed = parseGitignore(`${content}\n`)
+    const globs = parsed.globs()
+
+    for (const glob of globs) {
+      if (glob.type === 'ignore') ignores.push(...glob.patterns)
+      if (glob.type === 'unignore') ignores.push(...glob.patterns.map((pattern) => `!${pattern}`))
+    }
+  }
+
+  return { ignores }
 }
---
+++ eslint.config.js
@@ -0,0 +1,5 @@
+const config = require('./eslint-config')
+
+module.exports = config({
+  ignoreFiles: ['.gitignore'],
+})
